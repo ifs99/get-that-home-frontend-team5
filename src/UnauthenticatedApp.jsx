d11869052161cf8b6aa53ff8cc0d32bab05d8986
Italo Flores <ifloressar15>
2023-07-18 22:15:11 -0500

    Task:Header Component

--- src/UnauthenticatedApp.jsx
+++ src/UnauthenticatedApp.jsx
@@ -1,34 +1,29 @@
 import styled from "@emotion/styled";
-import React, { useState } from "react";
+import React, { useState , Component} from "react";
 import { colors } from "./styles";
-import React, { Component } from "react";
+
 
 import { default as ReactSelect } from "react-select";
 import NavBar from "./components/navbar";
 import Layout from "./components/Layout/Layout";
 import Input from "./components/ui/input";
 import Select from "./components/ui/select";
+import Header from "./components/Header/header";
 
 const Background = styled.img`
   width: 100%;
 `;
 
-const Header = styled.div`
-  
-`
 const MainContainer = styled.div`
   flex-grow: 1;
-`
+`;
 
-const Footer = styled.div`
-  
-`
+const Footer = styled.div``;
 
 function UnauthenticatedApp() {
   return (
     <Layout>
-      <Header>Hola soy el Header no autenticado</Header>
-      {/* Add routes here */}
+      <Header></Header>{/* Add routes here */}
       <MainContainer>Hola soy el Main Container</MainContainer>
       <Footer>Hola soy el footer</Footer>
     </Layout>
---
+++ src/components/logo.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const LogoImg = styled.img`
+  width:auto;
+`;
+
+function Logo() {
+  return (
+    <>
+      <LogoImg src="src/assets/GTHLogo.png" />
+    </>
+  );
+}
+export default Logo;
