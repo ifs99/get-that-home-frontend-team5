650b92d36166ed00887cf7bfe4043cd20cf1f5dc
CrhistianT <andrewturpo573@gmail.com>
2023-07-21 16:55:17 -0500

    new property

--- src/services/propertyServices.js
+++ src/services/propertyServices.js
@@ -4,6 +4,8 @@ export async function getProperties() {
   return await ApiFetch("/properties")
 }
 
-export async function createProperty({property_data}) {
-  
+export async function createProperty(property_data) {
+  return await ApiFetch("/properties", {
+    body: property_data
+  })
 }
